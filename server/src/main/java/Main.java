2cc009e723d2eff11f067ab404b7949d3d3aff42
jamesebrandt <brandtjamese@gmail.com>
2024-10-22 10:17:31 -0600

    Starting Spark Server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,17 @@
 import chess.*;
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+    }
+
+    public class simpleServer(){
+        port(8080);
+
+        get("hello", (req, res) -> "Hello World");
+
+
     }
 }
