1855d23e9e0189e36cf6146ba63f2a7c6aa9c4f7
jamesebrandt <brandtjamese@gmail.com>
2024-11-11 22:59:51 -0700

    implementing Repl

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -1,4 +1,41 @@
 package ui;
 
+import java.util.Arrays;
+
 public class PreLoginClient {
+
+
+    public String eval(String inputLine){
+        try {
+            var tokens = inputLine.toUpperCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "REGISTER" -> register(params);
+                case "LOGIN" -> login(params);
+                case "HELP" -> help();
+                case "QUIT" -> "quit";
+                default -> help();
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public String register(String... input){
+        return "not implemented";
+    }
+
+    public String login(String... input){
+        return "not implemented";
+    }
+
+    public String help() {
+        return """
+                - Register <username> <password> <email>
+                - Login <username> <password>
+                - Help
+                - Quit
+                """;
+    }
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,5 +1,8 @@
 package ui;
 
+import java.util.Scanner;
+import java.util.stream.StreamSupport;
+
 public class Repl{
     private final PreLoginClient preLoginClient;
     private final PostLoginClient postLoginClient;
@@ -12,9 +15,29 @@ public class Repl{
     }
 
     public void run(){
+        System.out.println("Welcome to the Chess Server! Sign in to Start");
+        System.out.print(preLoginClient.help());
 
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        result = result.toUpperCase();
 
+        while (!result.equals("QUIT")){
+            printPrompt();
+            String line = scanner.nextLine();
 
+            try {
+                result = preLoginClient.eval(line);
+                System.out.print(result);
+            } catch (Throwable e){
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
 
+    private void printPrompt() {
+        System.out.print("\n" + ">>> ");
     }
 }
