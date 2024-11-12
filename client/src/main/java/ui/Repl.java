3cff7528217d5574cbfd4aa1075144872d3ebf09
jamesebrandt <brandtjamese@gmail.com>
2024-11-11 22:32:48 -0700

    Started Proj 5 - Created Client classes

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,6 +1,20 @@
 package ui;
 
-public class Repl {
+public class Repl{
+    private final PreLoginClient preLoginClient;
+    private final PostLoginClient postLoginClient;
+    private final GameClient gameClient;
+
+    public Repl(String serverUrl){
+        preLoginClient = new PreLoginClient();
+        postLoginClient = new PostLoginClient();
+        gameClient = new GameClient();
+    }
+
+    public void run(){
 
 
+
+
+    }
 }
