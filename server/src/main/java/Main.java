df1595a6044b2364f89da5a603f128256962211d
jamesebrandt <brandtjamese@gmail.com>
2024-11-14 19:37:34 -0700

    code quality

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -5,10 +5,8 @@ public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
-
         Server server = new Server();
         server.run(0);
-
         System.out.print("Server is running on port");
     }
 }
