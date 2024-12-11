290fecec41412ed8a30fda21aa79484d9e8790ee
jamesebrandt <brandtjamese@gmail.com>
2024-12-11 11:48:15 -0700

    working on commands

--- shared/src/main/java/websocket/commands/RedrawBoardCommand.java
+++
@@ -1,8 +0,0 @@
-package websocket.commands;
-
-public class RedrawBoardCommand extends UserGameCommand {
-
-    public RedrawBoardCommand(String authToken, Integer gameID) {
-        super(CommandType.REDRAW_CHESS_BOARD, authToken, gameID);
-    }
-}
